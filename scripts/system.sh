67b09c679b4270b4b46a422ad273c18e49f314d9
Francis Park <francis.park@greenpeace.org>
2014-12-05 12:53:31 +1300

    Customising some alias entries for .bashrc

--- scripts/system.sh
+++ scripts/system.sh
@@ -30,7 +30,7 @@ export CLICOLOR=1
 export LSCOLORS=ExFxCxDxBxegedabagacad
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "\$(dircolors -b ~/.dircolors)" || eval "\$(dircolors -b)"
-    alias ls='ls --color=auto'
+    alias ll='ls -al'
     alias grep='grep --color=auto'
     alias fgrep='fgrep --color=auto'
     alias egrep='egrep --color=auto'
